8f46f6234ffb1f2ca2870843a456054b75064504
Peter Vawser <peter@catapult-elearning.com>
2013-01-04 13:14:58 +1030

    Modified attachment path

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -8,10 +8,10 @@ class AttachmentsController < ApplicationController
     klass = asset_class.constantize
     asset = klass.find(asset_id)
     
-    dir_name = Rails.root.join('attachments',asset_class.downcase.pluralize) 
+    dir_name = Rails.root.join('attachments',asset_class.downcase.pluralize,asset_id) 
     FileUtils.mkdir_p dir_name unless File.directory?(dir_name)
      
-    File.open(Rails.root.join('attachments',asset_class.downcase.pluralize,attach_file.original_filename), 'wb') do |file|
+    File.open("#{dir_name}/#{attach_file.original_filename}", 'wb') do |file|
       file.write(attach_file.read)
     end 
     
@@ -29,7 +29,7 @@ class AttachmentsController < ApplicationController
   
   def show
     attachment = Attachment.find(params[:id])
-    send_file Rails.root.join('attachments',attachment.asset_type.downcase.pluralize,attachment.doc_file_name), :type => attachment.doc_content_type
+    send_file Rails.root.join('attachments',attachment.asset_type.downcase.pluralize,attachment.asset_id.to_s,attachment.doc_file_name), :type => attachment.doc_content_type
     
     # klass = attachment.asset_type.constantize
     #    asset = klass.find(attachment.asset_id)
